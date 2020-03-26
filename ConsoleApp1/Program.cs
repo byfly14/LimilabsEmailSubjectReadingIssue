739c636e0346746d43e944df8c61ee98f79c4e84
Ян <kmita1996@gmail.com>
2020-03-26 16:02:19 +0300

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Limilabs.Mail;
+using Limilabs.Mail.MSG;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = "ImageTestDownload.msg";
+
+            IMail msg;
+
+            using (var converter = new MsgConverter(filePath))
+            {
+                msg = converter.CreateMessage();
+            }
+
+            Console.WriteLine(msg.Subject);
+            Console.ReadLine();
+        }
+    }
+}
